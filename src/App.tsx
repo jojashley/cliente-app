d817ef9ac14375f2d96bee7fb7755c51d751ee22
Jojashley Chaves <jojashley@hotmail.com>
2024-10-27 09:56:51 -0600

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Bienvenido a la compra de entradas</h1>
+      <div className="rectangle top">A</div>
+      <div className="rectangle left">D</div>
+      <div className="court">Cancha</div>
+      <div className="rectangle right">B</div>
+      <div className="rectangle bottom">C</div>
     </div>
   );
-}
+};
 
 export default App;
